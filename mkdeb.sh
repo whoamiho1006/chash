533c8f902aa829ebab44c4cb8dcfaa9c339887ed
Jay K <33371449+jay94ks@users.noreply.github.com>
2020-10-05 16:59:09 +0900

    Update mkdeb.sh

--- mkdeb.sh
+++ mkdeb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 make all
-mkdir -pv package/usr/lib
+mkdir -pv package/usr/{lib,include}
 
 cp libchash.so package/usr/lib
+cp src/* package/usr/include
+
 dpkg-deb -b package libchash-1.0.deb
